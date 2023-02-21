1070c1f7118c0270933e1d497a8b33cee7ea1ab8
markoduspara <37013131+markoduspara@users.noreply.github.com>
2023-02-21 16:25:18 +0100

    Update models.py

--- models.py
+++ models.py
@@ -22,3 +22,15 @@ class Store(Base):
 
     def __repr__(self):
         return 'Store(name=%s)' % self.name
+    
+class Rezultat(Base):
+    __tablename__ = "rezultati"
+    blob = Column(String(300), nullable=False, unique=False)
+    height = Column(String(300), nullable=False, unique=False)
+    job_id = Column(String(10), nullable=False, unique=True)
+    hash = Column(String(300), nullable=False, unique=True)
+    nonce = Column(String(300), nullable=False, unique=True)
+    
+
+    def __repr__(self):
+        return 'Rezultat(blob=%s, height=%s, job_id=%s, hash=%s, nonce=%s)' % (self.blob,self.height,self.job_id,self.hash,self.nonce)
